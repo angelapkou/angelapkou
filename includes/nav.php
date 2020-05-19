89c69a3e418e104efd83acd83b97dd14ffaffdc3
Angela <apk63@cornell.edu>
2020-05-18 20:57:22 -0400

    HANDWRITTING!

---
+++ includes/nav.php
@@ -0,0 +1,7 @@
+<nav>
+    <ul>
+      <li> <a href="index.php" class="<?php echo $about; ?>"><strong> About </strong></a> </li>
+      <li> <a href="projects.php" class="<?php echo $projects; ?>"><strong> Projects </strong></a> </li>
+      <li> <a href="art.php" class="<?php echo $art; ?>"><strong> Art </strong></a> </li>
+    </ul>
+  </nav>
---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+$about="nav";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<link rel="stylesheet" type="text/css" href="styles/style.css"/>
+
+<head>
+    <meta charset="UTF-8"/>
+    <meta name="viewport" content="width=device-width, intial-scale=1"/> 
+    <title>Angela's Portfolio</title>
+</head>
+
+<body>
+    <?php include("includes/nav.php");?>
+</body>
+</html>
