a684fd0a4d5323e0da719932ca5838675244c9fb
Angela <apk63@cornell.edu>
2020-05-18 21:52:06 -0400

    html

--- includes/nav.php
+++
@@ -1,7 +0,0 @@
-<nav>
-    <ul>
-      <li> <a href="index.php" class="<?php echo $about; ?>"><strong> About </strong></a> </li>
-      <li> <a href="projects.php" class="<?php echo $projects; ?>"><strong> Projects </strong></a> </li>
-      <li> <a href="art.php" class="<?php echo $art; ?>"><strong> Art </strong></a> </li>
-    </ul>
-  </nav>
