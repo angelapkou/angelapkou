f84196d064365484a714332fe0de00daeb309422
Angela <apk63@cornell.edu>
2020-05-18 23:04:38 -0400

    about

---
+++ projects.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<link rel="stylesheet" type="text/css" href="styles/style.css"/>
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" /> 
+    <title>Angela's Portfolio</title>
+</head>
+
+<body>
+    <nav>
+        <ul>
+            <li> <a href="index.php">About</a></li>
+            <li> <a href="projects.php" class="strike"><strong> Projects </strong></a> </li>
+            <li> <a href="art.php">Art</a> </li>
+        </ul>
+    </nav>
+
+    <div class="projectsection">
+        
+    </div>
+    
+</body>
+</html>
